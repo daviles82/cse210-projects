b17f7d7f6f395b20e76b7e81f691688918784cd9
daviles82 <avileshouston@live.com>
2023-02-25 17:05:07 -0600

    conditionals practice

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -2,8 +2,45 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+  static void Main(string[] args)
+  {
+    Console.Write("Please enter your grade. ");
+    string userGrade = Console.ReadLine();
+    int grade = int.Parse(userGrade);
+
+
+    string letter = "";
+
+    if (grade >= 90)
     {
-        Console.WriteLine("Hello Prep2 World!");
+      letter = "A";
     }
+    else if (grade >= 80)
+    {
+      letter = "B";
+    }
+    else if (grade >= 70)
+    {
+      letter = "C";
+    }
+    else if (grade >= 60)
+    {
+      letter = "D";
+    }
+    else
+    {
+      letter = "F";
+    }
+
+    Console.WriteLine($"You received a {letter}");
+
+    if (grade >= 70)
+    {
+      Console.WriteLine("You Passed!");
+    }
+    else
+    {
+      Console.WriteLine("Better luck next time!");
+    }
+  }
 }
