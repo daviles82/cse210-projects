6d4f872d8b3f6e94e9d234c673e67c8906ebd3e8
daviles82 <avileshouston@live.com>
2023-02-25 19:27:33 -0600

    random practice

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,8 +2,42 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+  static void Main(string[] args)
+  {
+    int guess = -1;
+
+    Random randomGenerator = new Random();
+    int number = randomGenerator.Next(1, 100);
+
+    while (guess != number)
     {
-        Console.WriteLine("Hello Prep3 World!");
+      Console.Write("Guess the right number. ");
+      string userGuess = Console.ReadLine();
+      guess = int.Parse(userGuess);
+
+      if (guess > number)
+      {
+        Console.WriteLine("Lower");
+      }
+      else if (guess < number)
+      {
+        Console.WriteLine("Higher");
+      }
+      else if (guess == number)
+      {
+        Console.WriteLine("You guessed right");
+        Console.WriteLine("Would you like to play another game? ");
+        string userPlayAgain = Console.ReadLine();
+
+        if (userPlayAgain == "yes")
+        {
+          number = randomGenerator.Next(1, 100);
+        }
+        else if (userPlayAgain == "no")
+        {
+          Console.WriteLine("Thank you for participating.");
+        }
+      }
     }
+  }
 }
