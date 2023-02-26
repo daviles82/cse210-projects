a1652b032be18f35f8eb572de31ef91e887659d6
daviles82 <avileshouston@live.com>
2023-02-25 22:43:11 -0600

    last update for week 1

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,56 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep5 World!");
-    }
+  static void Main(string[] args)
+  {
+
+    DisplayWelcomeMessage();
+
+    string userName = PromptUserName();
+    int userNumber = PromptUserNumber();
+    int SquredNumber = SqureNumber(userNumber);
+
+    DisplayResult(userName, SquredNumber);
+
+  }
+  static void DisplayWelcomeMessage()
+  {
+    Console.WriteLine("Welcome to the program!");
+  }
+
+  static string PromptUserName()
+  {
+    Console.Write("Please enter your name: ");
+    string userName = Console.ReadLine();
+
+    return userName;
+
+  }
+
+  static int PromptUserNumber()
+  {
+    Console.Write("Please enter your favorite number: ");
+    string userNumb = Console.ReadLine();
+    int userNumber = int.Parse(userNumb);
+
+    return userNumber;
+  }
+
+  static int SqureNumber(int userNumber)
+  {
+    int square = userNumber * userNumber;
+    return square;
+  }
+
+  static void DisplayResult(string userName, int square)
+  {
+    Console.WriteLine($"{userName}, the square of your number is {square}");
+  }
+
 }
+
+
+// Welcome to the program!
+// Please enter your name: Brother Burton
+// Please enter your favorite number: 42
+// Brother Burton, the square of your number is 1764
